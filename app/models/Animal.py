c86976830d03309746bfcc1c84867c9cfc92ac85
Jakson Bernardo <jaksondevweb@gmail.com>
2026-01-07 22:39:36 -0300

    CRIANDO AS CLASSES ANIMAL, CACHORRO E GATO

---
+++ app/models/Animal.py
@@ -0,0 +1,101 @@
+
+class Animal:
+
+    def __init__(self, id: int, especie: str, raca: str, nome: str,
+                 sexo: str, idade: int, porte: str, temperamento: str, 
+                 status: str) -> None:
+        
+        self._id = id
+        self._especie = especie
+        self._raca = raca
+        self._nome = nome
+        self._sexo = sexo
+        self._idade = idade
+        self._porte = porte
+        self._temperamento = temperamento
+        self._status = status
+
+    @property
+    def id(self) -> int: return self._id
+
+    @property
+    def especie(self) -> str: return self._especie
+
+    @property
+    def raca(self) -> str: return self._raca
+
+    @property
+    def nome(self) -> str: return self._nome
+
+    @property
+    def sexo(self) -> str: return self._sexo
+
+    @property
+    def idade(self) -> int: return self._idade
+
+    @property
+    def porte(self) -> str: return self._porte
+
+    @property
+    def temperamento(self) -> str: return self._temperamento
+
+    @property
+    def status(self) -> str: return self._status
+
+    @especie.setter
+    def especie(self, especie: str) -> None:
+        self._especie = especie
+
+    @raca.setter
+    def raca(self, raca: str) -> None:
+        self._raca = raca
+
+    @nome.setter
+    def nome(self, nome: str) -> None:
+
+        if not nome:
+            raise ValueError("O nome do animal não pode ser vazio.")
+        
+        self._nome = nome
+
+    @sexo.setter
+    def sexo(self, sexo: str) -> None:
+
+        if sexo not in ['M', 'F']:
+            raise ValueError("O sexo deve ser 'M' para macho ou 'F' para fêmea.")
+        
+        self._sexo = sexo
+
+    @idade.setter
+    def idade(self, idade: int) -> None:
+
+        if idade < 0:
+            raise ValueError("A idade do animal não pode ser negativa.")
+        
+        self._idade = idade
+
+    @porte.setter
+    def porte(self, porte: str) -> None:
+
+        if porte not in ['P', 'M', 'G']:
+            raise ValueError("O porte deve ser 'P' para pequeno, 'M' para médio ou 'G' para grande.")
+        
+        self._porte = porte
+
+    @temperamento.setter
+    def temperamento(self, temperamento: str) -> None:
+
+        self._temperamento = temperamento
+
+    @status.setter
+    def status(self, status: str) -> None:
+
+        if status not in ["DISPONIVEL", "RESERVADO", "ADOTADO", "DEVOLVIDO", "QUARENTENA", "INADOTAVEL"]:
+
+            raise ValueError("Status indisponível.")
+        
+        self._status = status
+
+    
+
+    
---
+++ app/models/Gato.py
@@ -0,0 +1,29 @@
+
+from .Animal import Animal
+
+class Gato(Animal):
+
+    def __init__(self, id: int, raca: str, nome: str, sexo: str, 
+                 idade: int, porte: str, temperamento: str, status: str, 
+                 necessidade_passeio: bool, independencia: bool) -> None:
+        
+        super().__init__(id, "Gato", raca, nome,
+                         sexo, idade, porte, temperamento, 
+                         status)
+        
+        self._necessidade_passeio = necessidade_passeio
+        self._independencia = independencia
+
+    @property
+    def necessidade_passeio(self) -> bool: return self._necessidade_passeio
+
+    @property
+    def independencia(self) -> bool: return self._independencia
+
+    @necessidade_passeio.setter
+    def necessidade_passeio(self, necessidade_passeio: bool) -> None:
+        self._necessidade_passeio = necessidade_passeio
+
+    @independencia.setter
+    def independencia(self, independencia: bool) -> None:
+        self._independencia = independencia
