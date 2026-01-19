367aac65c7c86fd45a2442ab5da30cb2119b9367
Jakson Bernardo <jaksondevweb@gmail.com>
2026-01-18 21:12:59 -0300

    ADICIONANDO CRUD COMPLETO DE CACHORRO

--- app/routes/CachorroRouters.py
+++ app/routes/CachorroRouters.py
@@ -1,29 +1,64 @@
 
-from fastapi import APIRouter
-from app.schemas.CachorroSchema import CachorroCreate, CachorroResponse
-from app.services.CachorroService import inserir_cachorro
+from fastapi import APIRouter, HTTPException
+from app.schemas.CachorroSchema import CachorroCreate, CachorroUpdate, CachorroResponse
+from app.services.CachorroService import inserir_cachorro, atualizar_cachorro, deletar_cachorro, ler_cachorro 
 
 cachorro_router = APIRouter(
     prefix = "/animal/cachorro",
     tags = ["Cachorro"]
 )
 
+@cachorro_router.get("/{id_animal}", response_model = CachorroResponse, status_code = 200)
+async def pesquisar_cachorro(id_animal: int):
+
+    try:
+
+        cachorro = await ler_cachorro(id_animal)
+
+        return cachorro
+    
+    except Exception as ex:
+
+        raise HTTPException(status_code = 500, detail = f"Erro ao pesquisar cachorro: {str(ex)}")
+    
 
 @cachorro_router.post("/", response_model = CachorroResponse, status_code = 201)
 async def criar_cachorro(animal: CachorroCreate):
 
-    novo_animal = await inserir_cachorro(animal)
+    try:
 
-    return {
-        "id": novo_animal.id,
-        "especie": novo_animal.especie,
-        "raca": novo_animal.raca,
-        "nome": novo_animal.nome,
-        "sexo": novo_animal.sexo,
-        "idade": novo_animal.idade,
-        "porte": novo_animal.porte,
-        "temperamento": novo_animal.temperamento,
-        "status": novo_animal.status,
-        "necessidade_passeio": novo_animal.necessidade_passeio,
-        "independencia": novo_animal.independencia
-    }
+        novo_animal = await inserir_cachorro(animal)
+
+        return novo_animal
+    
+    except Exception as ex:
+
+        raise HTTPException(status_code = 500, detail = f"Erro ao criar cachorro: {str(ex)}")
+    
+
+@cachorro_router.put("/{id_animal}", response_model = CachorroResponse, status_code = 200)
+async def alterar_cachorro(id_animal: int, animal: CachorroUpdate):
+
+    try:
+
+        cachorro = await atualizar_cachorro(id_animal, animal)
+
+        return cachorro
+
+    except Exception as ex:
+
+        raise HTTPException(status_code = 500, detail = f"Erro ao atualizar cachorro: {str(ex)}")
+    
+
+@cachorro_router.delete("/{id_animal}", status_code = 204)
+async def excluir_cachorro(id_animal: int):
+
+    try:
+
+        await deletar_cachorro(id_animal)
+
+    except Exception as ex:
+
+        raise HTTPException(status_code = 500, detail = f"Erro ao excluir cachorro: {str(ex)}")
+
+
--- app/schemas/CachorroSchema.py
+++ app/schemas/CachorroSchema.py
@@ -1,10 +1,29 @@
 from pydantic import BaseModel
+from typing import Optional, Literal
 from app.schemas.AnimalSchema import AnimalBase, AnimalResponse
-
 
 class CachorroCreate(AnimalBase):
     necessidade_passeio: bool
     independencia: bool
+
+class CachorroUpdate(BaseModel):
+    raca: Optional[str]
+    nome: Optional[str]
+    sexo: Optional[Literal["M", "F"]]
+    idade: Optional[int]
+    porte: Optional[Literal["P", "M", "G"]]
+    temperamento: Optional[str]
+    status: Optional[Literal[
+        "DISPONIVEL",
+        "RESERVADO",
+        "ADOTADO",
+        "DEVOLVIDO",
+        "QUARENTENA",
+        "INADOTAVEL"
+    ]]
+
+    necessidade_passeio: Optional[bool]
+    independencia: Optional[bool]
 
 
 class CachorroResponse(AnimalResponse):
